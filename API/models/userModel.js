f11c9de77ddbcc7ef3c305fc8f2ad3ac7676cd3c
SethJKallis <kallisseth@gmail.com>
2023-02-21 11:09:53 +0200

    implemented password hashing with bcrypt

--- API/models/userModel.js
+++ API/models/userModel.js
@@ -1,4 +1,6 @@
 const db = require('../config/db.connection');
+const bcrypt = require('bcrypt');
+
 
 module.exports = {
     getUsers(result){
@@ -19,17 +21,26 @@ module.exports = {
         });
     },
 
-    insertUser(data, result){
-        db.query(`INSERT INTO users SET ?`, [data], (err,results) => {
+    async insertUser(data, result){
+        const pass = data.user_pass;
+        bcrypt.hash(pass, 10, (err,hash) => {
             if(err){
                 console.log(err);
-                result(err, null);
-            } else result(null, results);
-        });
+                result(err, null)
+            } else {
+                data.user_pass = hash
+                db.query(`INSERT INTO users SET ?`, [data], (err,results) => {
+                    if(err){
+                        console.log(err);
+                        result(err, null);
+                    } else result(null, results);
+                });
+            }
+        })
     },
 
     updateUser(data, id, result){
-        db.query(`UPDATE users SET user_name = ?, user_email = ?, user_password WHERE user_id = ?`, [data.user_name, data.user_email, data.user_password, id], (err,results) => {
+        db.query(`UPDATE users SET user_name = ?, user_email = ?, user_pass = ? WHERE user_id = ?`, [data.user_name, data.user_email, data.user_pass, id], (err,results) => {
             if(err){
                 console.log(err);
                 result(err,null);
@@ -44,6 +55,29 @@ module.exports = {
                 result(err, null);
             } else result(null, results)
         });
+    },
+
+    async userLogin(user_email, user_pass, result){
+        db.query(`SELECT * FROM users WHERE user_email = ?`, [user_email], (err,rows) => {
+            if(err){
+                console.log(err);
+                result(err,null);
+            } else if (rows.length == 0){
+                result(null, {status: "Error", message: "User not found"})
+            } else {
+                const user = rows[0];
+                bcrypt.compare(user_pass, user.user_pass, (err,res) => {
+                    if(err){
+                        console.log(err);
+                        result(err,null);
+                    } else if(res){
+                        result(null, {status: "success", user: user})
+                    } else {
+                        result(null, {status: "error", message: "incorrect password"})
+                    }
+            })
+            }
+        })
     }
 
 }
